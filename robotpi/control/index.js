3140b66ec8c19e5afcd833d6b4536ec4d35c8c22
pontusstjerna <pontusstjernstrom@gmail.com>
2018-06-14 09:53:43 +0200

    Removed a line

--- robotpi/control/index.js
+++ robotpi/control/index.js
@@ -1,7 +1,6 @@
 import PythonShell from 'python-shell';
 
 const shell = new PythonShell('python/controller.py');
-shell.send('setMotorRightt(23)');
 
 shell.on('message', message => console.log('From Python: ' + message));
 
