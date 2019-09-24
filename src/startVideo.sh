5fcd484e7c56c95dbddc168cc1b27256dec5f974
pontusstjerna <pontusstjernstrom@gmail.com>
2019-09-24 21:31:27 +0200

    Changed exposure setting

--- src/startVideo.sh
+++ src/startVideo.sh
@@ -1,1 +1,1 @@
-avconv -s 640x480 -f video4linux2 -i /dev/video0 -f mpegts -codec:v mpeg1video -codec:a mp2 -b 1000k exposure_auto=1 http://localhost:4000/stream
+avconv -s 640x480 -f video4linux2 -i /dev/video0 -f mpegts -codec:v mpeg1video -codec:a mp2 -b 1000k http://localhost:4000/stream
