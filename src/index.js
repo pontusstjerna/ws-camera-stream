e6a67d7e6967007dd18b347b43404630d4a77761
pontusstjerna <pontusstjernstrom@gmail.com>
2019-09-27 19:53:01 +0200

    Actually added a http server for listening to local streams.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import http from 'http';
 import { config } from 'dotenv';
 import start from './video';
 
@@ -8,5 +9,9 @@ const PORT = process.env.PORT || 4000;
 
 console.log('Starting stream server...');
 const app = express();
+const server = http.Server(app);
 
-start(app, PORT);
+server.listen(4000, () => {
+    start(app, PORT);
+    console.log(': Base server successfully started on port ' + PORT);
+});
